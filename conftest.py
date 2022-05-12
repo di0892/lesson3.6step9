5c0e730b5bfc3e14cc34875741f339af749b9f5a
dinara.it <dinara.it@roxot.com>
2022-05-12 17:41:01 +0700

    done

--- conftest.py
+++ conftest.py
@@ -3,13 +3,28 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
-    parser.addoption('--languages', action='store', default=None,
-                     help="Choose language")
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose language: ru or en")
 
 @pytest.fixture(scope="function")
 def browser(request):
-    options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-    browser = webdriver.Chrome(options=options)
+    browser_name = request.config.getoption("browser_name")
+    language = request.config.getoption("language")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart browser for test..")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        print("\nstart browser for test..")
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
+    print("\nquit browser..")
     browser.quit()
--- test_items.py
+++ test_items.py
@@ -1,6 +1,12 @@
+import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_guest_should_see_login_link(browser):
+def test_find_button_add_to_cart(browser):
     browser.get(link)
-    browser.find_elements_by_css_selector("btn-add-to-basket")
-    print("\nfound button")
+    # time.sleep(30)
+    try:
+        button = browser.find_element_by_css_selector("button.btn-add-to-basket")
+    except:
+        assert False, "Button add to cart is not found!"
+
+# pytest -s -v --language=fr test_lesson36_9/test_items.py
